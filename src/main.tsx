bc34691578bf37ffe4e293422e8681707c10e6ee
hadis <hadissdehbashii@gmail.com>
2026-08-14 17:42:39 +0330

    feat: implement layout and routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./components/router";
+import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import "@fontsource/inter/400.css";
