4f2f70cf7e8aae4ff9052dfa7b9469ca26c1b44a
delaram <delaram.zed90@gmail.com>
2026-08-15 19:48:48 +0330

    feat: create RecommendedUserCard UI

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,20 +1,24 @@
 // import PostCard from "../../src/Components/PostCard";
 import AddPostCard from "../Components/AddPostCard";
+import RecommendedUserCard from "../Components/RecommendedUserCard";
 
 function Home() {
   return (
     <>
-     <div>home page</div>
-       <div className="min-h-screen bg-secondary-content px-4 py-8">
-      <main className="flex justify-center">
-        {/* <PostCard /> */}
-        <AddPostCard/>
-      </main>
-    </div>
-    </>
- 
+      <div>home page</div>
 
-);
+      <div className="mx-auto flex w-full max-w-6xl gap-6 px-4 flex-row">
+        <main className="flex flex-col w-full ">
+          <AddPostCard />
+          {/* <PostCard/> */}
+        </main>
+
+        <aside className="w-96 md:shrink-0">
+          <RecommendedUserCard />
+        </aside>
+      </div>
+    </>
+  );
 }
 
 export default Home;
