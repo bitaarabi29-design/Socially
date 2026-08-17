0746c96b8aa57d69ca3a14e2c3aa86b47d6e5f06
hamed-abbassi <abbassi1379.ha@gmail.com>
2026-08-17 16:31:03 +0330

    feat: add responsive header and mobile sidebar

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import MobileSidebar from "./MobileSidebar";
 
 function Layout() {
   return (
     <div>
-      <header>Header</header>
+     <Header />
+     <MobileSidebar />
       <main>
         <aside>Left Sidebar</aside>
         <section>
