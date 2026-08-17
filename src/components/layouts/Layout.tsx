44d0d29755cb54a6eddf4c535978d3f04f0aa957
hadis <hadissdehbashii@gmail.com>
2026-08-17 17:00:33 +0330

    fix(layout): correct layout structure and remove redundant imports

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -2,26 +2,23 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import MobileSidebar from "./MobileSidebar";
 import SideSignInCard from "../cards/SideSignInCard";
-import Container from "../Ui/Container";
+import SideProfileCard from "../cards/SideProfileCard";
 
 
 
 function Layout() {
   return (
     <div>
-
        <Header />
      <MobileSidebar />
-      <main className="mx-auto grid grid-cols-5 gap-3 max-w-7xl px-4">
+      <main className="mx-auto grid grid-cols-5 gap-3 max-w-7xl ph-4">
          <aside className=" md:shrink-0 col-span-1">
           <SideSignInCard />
+          <SideProfileCard />
          </aside>
-        <section className="col-span-4">
-          
+         <section className="col-span-4">
              <Outlet />
-          
-
-        </section>
+         </section>
       </main>
     </div>
   );
