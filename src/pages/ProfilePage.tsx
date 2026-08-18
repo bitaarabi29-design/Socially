85e315d65318a058a50c28d73041699aaae1f264
Bita Arabi <bitaarabi29@gmail.com>
2026-08-18 18:37:31 +0330

    feat: responsive profile and home page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,22 +1,30 @@
-import PostCard from "../components/cards/PostCard";
-import RecommendedUserCard from "../components/cards/RecommendedUserCard";
-import UserProfileCard from "../components/cards/UserProfileCard";
-import Container from "../components/Ui/Container";
-
+import PostCard from "../Components/cards/PostCard";
+import RecommendedUserCard from "../Components/cards/RecommendedUserCard";
+import UserProfileCard from "../Components/cards/UserProfileCard";
+import Container from "../Components/Ui/Container";
 
 function Profile() {
-
   return (
-     <Container>
-        <div className="col-span-3 flex flex-col gap-6">
-          <UserProfileCard user={{ id: "1", name: "John Doe", username: "johndoe", bio: "Software developer", location: "New York", followingCount: 100, followerCount: 500, isCurrentUser: true }} />
-          <PostCard />
-        </div>
-         
-        <div className="col-span-2 flex flex-col gap-6">
-          <RecommendedUserCard />
-        </div>
-    
+    <Container>
+      <div className="col-span-5 flex flex-col gap-6 md:col-span-3">
+        <UserProfileCard
+          user={{
+            id: "1",
+            name: "John Doe",
+            username: "johndoe",
+            bio: "Software developer",
+            location: "New York",
+            followingCount: 100,
+            followerCount: 500,
+            isCurrentUser: true,
+          }}
+        />
+        <PostCard />
+      </div>
+
+      <div className="hidden md:col-span-2 md:flex md:flex-col md:gap-6">
+        <RecommendedUserCard />
+      </div>
     </Container>
   );
 }
