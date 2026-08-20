e8bfdbb8670451bdb77c786888e4f12e502ed7ad
Bita Arabi <bitaarabi29@gmail.com>
2026-08-20 22:11:19 +0330

    feat: adding api

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -2,18 +2,18 @@ import api from "../lib/axios";
 import type { loginTypes, registerTypes } from "../types/authTypes";
 
 export const loginRequest = (data: loginTypes) => {
-  return api.post("/authentication/login", data);
+  return api.post("api/authentication/login", data);
 };
 
 export const registerRequest = (data: registerTypes) => {
-  return api.post("/authentication/register", data);
+  return api.post("api/authentication/register", data);
 };
 
 export const getSessionRequest = async () => {
-  const response = await api.post("/authentication/session");
+  const response = await api.post("api/authentication/session");
   return response.data;
 };
 
 export const logoutRequest = (data: loginTypes) => {
-  return api.post("/authentication/logout", data);
+  return api.post("api/authentication/logout", data);
 };
