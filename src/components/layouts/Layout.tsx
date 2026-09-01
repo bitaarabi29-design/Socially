9b1bc952f9d28dac7517294465f2cbdf43cd41fa
Bita Arabi <bitaarabi29@gmail.com>
2026-09-01 12:50:30 +0330

    feat: add loading page

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -22,32 +22,35 @@ function Layout() {
     );
   }
 
-  const { data: session ,isLoading} = useSession();
+  const { data: session, isLoading } = useSession();
   const hasSession = !!session;
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center gap-3">
+        <p className="text-base-content/60 text-3xl">Loading</p>
+
+        <div className="flex gap-2">
+          <div className="bg-primary h-3 w-3 animate-bounce rounded-full [animation-delay:-0.3s]"></div>
+          <div className="bg-primary h-3 w-3 animate-bounce rounded-full [animation-delay:-0.15s]"></div>
+          <div className="bg-primary h-3 w-3 animate-bounce rounded-full"></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header theme={theme} toggleTheme={toggleTheme} />
 
       <MobileSidebar theme={theme} toggleTheme={toggleTheme} />
-      
-        
-      
-       
+
       <main className="mx-auto grid max-w-7xl grid-cols-1 gap-6 p-4 md:grid-cols-5">
         <aside className="lg:col-span-1 lg:flex lg:shrink-0 lg:flex-col lg:gap-6">
+          {hasSession ? <SideProfileCard /> : <SideSignInCard />}
+        </aside>
 
-        
-  {isLoading ? (
-    <div>Loading...</div>
-  ) : hasSession ? (
-    <SideProfileCard />
-  ) : (
-    <SideSignInCard />
-  )}
-</aside>
-        
         <section className="col-span-1 md:col-span-4">
-
           <Outlet />
         </section>
       </main>
